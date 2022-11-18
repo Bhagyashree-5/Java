3913453ffc3b1e39b1201035574309a8b554edaf
Bhagyashree-5 <111446245+Bhagyashree-5@users.noreply.github.com>
2022-11-18 16:09:09 +0530

    Add files via upload

---
+++ Lab17-11-22/StatementProg.java
@@ -0,0 +1,58 @@
+package Lab;
+
+import java.sql.*;
+import java.io.*;
+
+public class StatementProg {
+	static Connection conn;
+	static BufferedReader br;
+	
+	public void createTable() throws SQLException {
+		// creating table using Statement interface and its methods
+		Statement stmt = conn.createStatement();
+		stmt.executeUpdate("CREATE TABLE PSG(psgid int PRIMARY KEY auto_increment, psgname varchar(30), psgemail varchar(255))");
+		System.out.println("Table created.");
+	}
+	
+	public void insertRows() throws SQLException, IOException {
+		PreparedStatement pstmt = conn.prepareStatement("INSERT INTO PSG(psgname, psgemail) VALUES(?,?)");
+		System.out.print("Write passenger name: ");
+		String psgName = br.readLine();
+		pstmt.setString(1, psgName);
+		System.out.print("Write passenger email: ");
+		String psgEmail = br.readLine();
+		pstmt.setString(2, psgEmail);
+		int rows = pstmt.executeUpdate();
+		System.out.println(rows + " row inserted");
+	}
+	
+	public static void main(String[] args) {
+		try {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "bhagyashree", "Bhagyashree07");
+			StatementProg prg = new StatementProg();
+			do {
+				System.out.print("Enter '1' to create table or '2' to insert rows: ");
+				int userChoice = Integer.parseInt(br.readLine());
+				switch (userChoice) {
+				case 1:
+					prg.createTable();
+					break;
+				case 2:
+					prg.insertRows();
+					break;
+				default:
+					System.out.println("Wrong input! Try again");
+				}
+				System.out.print("Do you want to continue with the program? Type(yes/no): ");
+				String isContinue = br.readLine().toLowerCase();
+				if (isContinue.equals("no")) {
+					break;
+				}
+			} while (true);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
